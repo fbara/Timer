1064b4f4c4583fe07dffcc6da5be514c952089a5
fbara <fbara1@yahoo.com>
2021-09-25 18:22:53 -0500

    Add TimerCell

---
+++ Timer WatchKit Extension/Views/TimerCell.swift
@@ -0,0 +1,20 @@
+//
+//  TimerCell.swift
+//  Timer WatchKit Extension
+//
+//  Created by Frank Bara on 9/25/21.
+//
+
+import SwiftUI
+
+struct TimerCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerCell_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerCell()
+    }
+}
